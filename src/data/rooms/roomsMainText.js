b7ec7d217fdb556de8b9de636088e398f90e2508
gzm <gzm.nyc@gmail.com>
2023-05-06 15:05:11 +0900

    minor change in roomsMainTet.js

--- src/data/rooms/roomsMainText.js
+++ src/data/rooms/roomsMainText.js
@@ -4,7 +4,7 @@ export const roomsMainText = [
 		id: 0,
 		category: 'rooms',
 		name: 'Rooms Description',
-		text: 'Hotel Sejour Mint has a range of amenities and facilities to ensure that guests have a comfortable and enjoyable stay. With spacious common areas, as well as several types of guest rooms for individual, family or group travelers, where guests can relax and unwind after  activities. The hotel also offers complimentary Wi-Fi service throughout the property, ensuring that guests can stay connected while on the go.',
+		text: 'Hotel Sejour Mint has a range of amenities and facilities to ensure that guests have a comfortable and enjoyable stay. With several types of guest rooms for individual, family or group travelers, and spacious common areas, guests can relax and unwind after activities. The hotel offers Wi-Fi service throughout the property, ensuring that guests can stay connected while on the go.',
 		textJ: 'ホテル セジュール・ミントには、快適で楽しい滞在を提供するための様々なアメニティや設備があります。広々とした共用エリアや個人旅行者、家族旅行者、グループ旅行者向けに用意された様々なタイプの客室があり、アクティビティの後にゲストはくつろいでリラックスすることができます。また、無料のWi-Fiサービスも提供しており、移動中でもゲストが常にインターネットに接続できるようになっています。'
 	},
 	{
